57a1b724df8ac15dad336ab112963bbba5d99572
Турсунов Баходур <tursunovb18@gmail.com>
2024-06-21 11:21:06 +0500

    added solition keep up the hoop

---
+++ 8kyu Keep up the hoop/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace _8kyu_Keep_up_the_hoop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(HoopCount(5));
+            Console.WriteLine(HoopCount(20));
+        }
+
+        public static string HoopCount(int n)
+        {
+            if (n >= 10) return "Great, now move on to tricks";
+            else return "Keep at it until you get it";
+        }
+    }
+}
