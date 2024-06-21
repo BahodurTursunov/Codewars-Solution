64268f62e538729b75a8507cf6ae44b8bae2077a
Турсунов Баходур <tursunovb18@gmail.com>
2024-06-21 11:00:20 +0500

    added project uppercase

---
+++ 8kyu MakeUpperCase/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace _8kyu_MakeUpperCase
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(MakeUpperCase("hello world!"));
+        }
+
+        public static string MakeUpperCase(string str)
+        {
+            return str.ToUpper();
+        }
+    }
+}
