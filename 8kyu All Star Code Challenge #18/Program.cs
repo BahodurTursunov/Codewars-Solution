a5354420a37a9646328616ace9448d1c99af8d23
Турсунов Баходур <tursunovb18@gmail.com>
2024-06-21 09:41:49 +0500

    added gitignore and solution

---
+++ 8kyu All Star Code Challenge #18/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _8kyu_All_Star_Code_Challenge__18
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(StrCount("Hello world", 'o'));
+        }
+        public static int StrCount(string str, char letter)
+        {
+            int count = 0;
+            foreach (var c in str)
+            {
+                if (c == letter)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+
+
+
+}
+
---
+++ Codewars Solutions/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Codewars_Solutions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
