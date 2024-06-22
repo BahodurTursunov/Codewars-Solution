c5e769b0dd74d89281577cbc1825a6cfa3e5741c
Турсунов Баходур <tursunovb18@gmail.com>
2024-06-22 21:02:23 +0500

    added true or false solution

---
+++ 8kyu Chuck Norris VII - True or False_Beginner/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace _8kyu_Chuck_Norris_VII___True_or_False_Beginner
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        public static bool IfChuckSaysSo()
+        {
+            return false;
+        }
+    }
+}
