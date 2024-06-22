3252ccd3c4a9ca1646361e38ca67c444623f8e5f
Турсунов Баходур <tursunovb18@gmail.com>
2024-06-22 23:50:30 +0500

    add

---
+++ 8kyu Is it even/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace _8kyu_Is_it_even
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        public bool IsEven(double n) //=> n % 2 == 0;
+        {
+            if (n % 2 == 0)
+                return true;
+            else if (n == 0)
+                return true;
+            else
+                return false;
+
+            //return (n % 2 == 0) ? true : false;
+        }
+    }
+}
---
+++ 8kyu get character from ASCII Value/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace _8kyu_get_character_from_ASCII_Value
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetChar(65));
+            Console.WriteLine(GetChar(97));
+            Console.WriteLine(GetChar(96));
+        }
+        public static char GetChar(int charcode) // => (char)charcode;
+        {
+            return Convert.ToChar(charcode);
+            // return (char)charcode;
+        }
+    }
+}
