fe4fa26f575054698234c7ad4efd1cc851131c46
Турсунов Баходур <tursunovb18@gmail.com>
2024-07-26 15:15:44 +0500

    add 6kyu solution. Count

---
+++ 6kyu Count characters in your string/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace _6kyu_Count_characters_in_your_string
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string input = "aba";
+            var result = Count(input);
+
+            foreach (var kvp in result)
+            {
+                Console.Write($"{kvp.Key}: {kvp.Value} ");
+            }
+        }
+
+        public static Dictionary<char, int> Count(string str)
+        {
+            var characterCount = new Dictionary<char, int>();
+
+            foreach (char c in str)
+            {
+                if (characterCount.ContainsKey(c))
+                {
+                    characterCount[c]++;
+                }
+                else
+                {
+                    characterCount[c] = 1;
+                }
+            }
+
+            return characterCount;
+        }
+    }
+}
+
+
+#region Can Be
+/* 
+ * return str.GroupBy(c => c).ToDictionary(g => g.Key, g => g.Count());
+ */
+
+/*
+   public static Dictionary<char, int> Count(string str) => str.GroupBy(c => c).ToDictionary(g => g.Key, g => g.Count());
+ */
+#endregion
