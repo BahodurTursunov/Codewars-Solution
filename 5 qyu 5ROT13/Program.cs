6a91d0a14227815aee7fc471b9753265842650e9
b_tursunov <b_tursunov@01-B-TUSUNOV>
2025-04-10 08:50:33 +0500

    added new solutions

---
+++ 5 qyu 5ROT13/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _5qyu_5ROT13
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine(Kata.Rot13("Va gur ryringbef, gur rkgebireg ybbxf ng gur BGURE thl'f fubrf."));
+        }
+
+        public class Kata
+        {
+            public static string Rot13(string input)
+            {
+                return new string(input.Select(c =>
+                {
+                    if (c >= 'a' && c <= 'z')
+                    {
+                        return (char)('a' + ((c - 'a' + 13) % 26));
+                    }
+
+                    if (c >= 'A' && c <= 'Z')
+                    {
+                        return (char)('A' + ((c - 'A' + 13) % 26));
+                    }
+
+                    return c;
+                }).ToArray());
+            }
+        }
+
+    }
+}
---
+++ 6kyu Highest Rank Number in an Array/Program.cs
@@ -0,0 +1,61 @@
+﻿namespace _6kyu_Highest_Rank_Number_in_an_Array
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            int[] arr = { 12, 10, 8, 12, 7, 6, 4, 10, 12 };
+
+            var res = HighestRank(arr);
+
+            Console.WriteLine(res);
+        }
+
+
+        /* public static int HighestRank(int[] arr)
+         {
+             Dictionary<int, int> frequency = new Dictionary<int, int>();
+
+             foreach (var num in arr)
+             {
+                 if (frequency.ContainsKey(num))
+                 {
+                     frequency[num]++;
+                 }
+                 else
+                 {
+                     frequency[num] = 1;
+                 }
+             }
+
+             int maxFrequency = frequency.Values.Max();
+             int mostFrequent = int.MinValue;
+
+             foreach (var pair in frequency)
+             {
+                 if (pair.Value == maxFrequency)
+                 {
+                     mostFrequent = Math.Max(mostFrequent, pair.Key);
+                 }
+             }
+
+             return mostFrequent;
+         }
+         */
+        #region LINQ
+
+        public static int HighestRank(int[] arr)
+        {
+            return arr
+              .GroupBy(i => i)
+              .OrderByDescending(gr => gr.Count())
+              .ThenByDescending(gr => gr.Key)
+              .Select(gr => gr.Key)
+              .First();
+        }
+        #endregion
+
+
+
+    }
+}
